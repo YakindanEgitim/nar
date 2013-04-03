04daad70d08c31e0dcb56dcfaa0cde1bd5102885
ferhat elmas <elmas.ferhat@gmail.com>
2013-04-03 17:14:32 +0200

    better setup

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
+import os
 from setuptools import setup, find_packages
+
+
+def get_content(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
 setup(
     name = "nar",
     version = "0.0.1",
     url = "https://github.com/YakindanEgitim/nar",
-    description = "New Album Reporter",
-    author = "Ferhat Elmas, Mesutcan Kurt",
     license = "GPLv3",
+    description = "New Album Reporter",
+    long_description = get_content('README.md'),
+
+    author = "Ferhat Elmas, Mesutcan Kurt",
+    author_email = "elmas.ferhat@gmail.com",
+
     packages = find_packages('src'),
     package_dir = {'': 'src'},
-    install_requires = ['setuptools',
-                        'django',
-                        'django-social-auth',
-                        'django-bootstrap-toolkit'],
+    install_requires = ['setuptools'],
 )
