f2d317c8acf7bb2b6aa876d1d8fe0564a489b11d
ferhat elmas <elmas.ferhat@gmail.com>
2013-04-15 20:04:05 +0200

    s3 for static

--- nar/settings/base.py
+++ nar/settings/base.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(os.path.dirname(__file__), "../../db"),
+        'NAME': os.environ.get("TMPDIR", "/tmp/nar-db"),
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
@@ -20,31 +20,14 @@ DATABASES = {
     }
 }
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'Europe/Zurich'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
@@ -82,7 +65,11 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
+# For now, it is quite ok but later we must remove secret from git
+# See ../../scripts/generate_secret_key
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 SECRET_KEY = 'mqu6)15)-f==q6b#1nyjw5cevb**)6hro5hl@bjl#*hv!kbkls'
+
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -151,6 +138,16 @@ FACEBOOK_API_SECRET = ''
 LOGIN_URL = '/accounts/'
 LOGIN_REDIRECT_URL = '/'
 LOGIN_ERROR_URL = '/login-error/'
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_auth.backends.pipeline.social.social_auth_user',
+    # 'social_auth.backends.pipeline.associate.associate_by_email', security issues
+    'social_auth.backends.pipeline.user.get_username',
+    'social_auth.backends.pipeline.user.create_user',
+    'social_auth.backends.pipeline.social.associate_user',
+    'social_auth.backends.pipeline.user.update_user_details',
+    'profiles.pipelines.get_user_avatar',
+)
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
--- nar/settings/prod.py
+++ nar/settings/prod.py
@@ -1,8 +1,25 @@
 import dj_database_url
 from .base import *
 
+# Close debug
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+# When DEBUG is False, empty ALLOWED_HOSTS isn't permitted
+ALLOWED_HOSTS = ['nar.herokuapp.com']
+
 # Parse database configuration from $DATABASE_URL
 DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'nar'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
