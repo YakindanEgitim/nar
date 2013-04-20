3c4482742afe2f380512ca281383312aa17e18ce
ferhat elmas <elmas.ferhat@gmail.com>
2013-04-20 19:02:41 +0200

    base abstract model for all models
    related #2, #3, #4, #5, #6, #9, #16

--- core/models.py
+++ core/models.py
@@ -1,1 +1,14 @@
 # core/models.py
+from django.db import models
+
+
+class AbstractTimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides
+    self-updating fields ``created`` and ``modified``
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
