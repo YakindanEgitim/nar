5d9b9d22d942cbdc0284d0da7c5fd0794d500d37
ferhat elmas <elmas.ferhat@gmail.com>
2013-04-27 16:34:06 +0200

    update urlconf for new apps

--- nar/urls.py
+++ nar/urls.py
@@ -1,10 +1,15 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'', include('social_auth.urls')),
+    url(r'^', include('social_auth.urls')),
+    url(r'^', include('core.urls')),
+    url(r'^profiles/', include('profiles.urls')),
     url(r'^', include('aas.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^photologue/', include('photologue.urls')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
