4bf529d4a80538733a46aafb24ed5849af34fed1
ferhat elmas <elmas.ferhat@gmail.com>
2013-05-01 22:41:00 +0200

    different sized avatars

--- profiles/models.py
+++ profiles/models.py
@@ -6,6 +6,9 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+
 from core.models import AbstractTimeStampedModel
 from nar.settings.base import MEDIA_ROOT
 
@@ -27,6 +30,18 @@ def get_upload_path(instance, filename):
 
 class Profile(AbstractUser, AbstractTimeStampedModel):
     avatar = models.ImageField(upload_to=get_upload_path, blank=True)
+    avatar_thumbnail = ImageSpecField(image_field='avatar',
+                                      processors=[ResizeToFill(100, 50)],
+                                      format='JPEG',
+                                      options={'quality': 60})
+    avatar_medium = ImageSpecField(image_field='avatar',
+                                   processors=[ResizeToFill(200, 100)],
+                                   format='JPEG',
+                                   options={'quality': 60})
+    avatar_large = ImageSpecField(image_field='avatar',
+                                  processors=[ResizeToFill(400, 200)],
+                                  format='JPEG',
+                                  options={'quality': 60})
 
     def is_artist(self):
         try:
