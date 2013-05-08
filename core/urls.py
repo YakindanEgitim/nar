8a7a75c624fdfb145e83a908d2ae05929ab2c058
ferhat elmas <elmas.ferhat@gmail.com>
2013-05-08 20:16:00 +0200

    initial search

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 
-from .views import AboutView, ContactView, ThanksView
+from .views import AboutView, ContactView, SearchView, ThanksView
 
 urlpatterns = patterns('',
     url(r'^about/$', AboutView.as_view(), name='about'),
     url(r'^contact/$', ContactView.as_view(), name='contact'),
     url(r'^thanks/$', ThanksView.as_view(), name='thanks'),
+    url(r'^search/$', SearchView.as_view(), name='search'),
 )
--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 # core/views.py
-from django.views.generic import FormView, TemplateView
+from django.views.generic import FormView, ListView, TemplateView
+
+from music.models import Album, Song
+from profiles.models import Artist
 from .forms import ContactForm
 
 class AboutView(TemplateView):
@@ -18,3 +21,16 @@ class ContactView(FormView):
     def form_valid(self, form):
         # form.send_email()
         return super(ContactView, self).form_valid(form)
+
+
+class SearchView(ListView):
+    template_name = "core/search.html"
+    queryset = Artist.objects.none()
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchView, self).get_context_data(**kwargs)
+        q = self.request.GET.get('q')
+        context["artist_list"] = Artist.objects.filter(profile__username__icontains=q)
+        context["album_list"] = Album.objects.filter(name__icontains=q)
+        context["song_list"] = Song.objects.filter(name__icontains=q)
+        return context
