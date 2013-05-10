8faabdaaea0065cef42eeb5545e33df4b193f031
ferhat elmas <elmas.ferhat@gmail.com>
2013-05-10 21:55:00 +0200

    override object getters in music handling views

--- music/views.py
+++ music/views.py
@@ -1,6 +1,7 @@
 # music/views.py
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.views.generic import CreateView, DetailView, UpdateView
 from django.utils.translation import ugettext as _
 
@@ -17,7 +18,7 @@ class ArtistDetailView(DetailView):
     template_name = "music/artist_detail.html"
 
     def get_object(self, queryset=None):
-        pass
+        return get_object_or_404(Artist, profile__username=self.kwargs["username"])
 
 
 class ArtistUpdateView(LoginRequiredMixin, ActionMixin, UpdateView):
@@ -26,10 +27,10 @@ class ArtistUpdateView(LoginRequiredMixin, ActionMixin, UpdateView):
     action = _("Artist is successfully updated")
 
     def get_object(self, queryset=None):
-        pass
+        return get_object_or_404(Artist, profile__username=self.kwargs["username"])
 
     def get_success_url(self):
-        pass
+        return reverse('artist_detail', self.kwargs)
 
 
 class ArtistCreateView(LoginRequiredMixin, ActionMixin, CreateView):
@@ -41,7 +42,7 @@ class ArtistCreateView(LoginRequiredMixin, ActionMixin, CreateView):
     def form_valid(self, form):
         pass
 
-        
+       
 class AlbumCreateView(LoginRequiredMixin, ActionMixin, CreateView):
     model = Album
     form_class = AlbumForm
@@ -79,7 +80,9 @@ class AlbumDetailView(DetailView):
     template_name = "music/album_detail.html"
 
     def get_object(self, queryset=None):
-        pass
+        return get_object_or_404(Album,
+                                 slug=self.kwargs["album"],
+                                 artist__profile__username=self.kwargs["username"])
 
 
 class AlbumUpdateView(LoginRequiredMixin, ActionMixin, UpdateView):
@@ -88,10 +91,12 @@ class AlbumUpdateView(LoginRequiredMixin, ActionMixin, UpdateView):
     action = _("Album is successfully updated")
 
     def get_object(self, queryset=None):
-        pass
+        return get_object_or_404(Album,
+                                 slug=self.kwargs["album"],
+                                 artist__profile__username=self.kwargs["username"])
 
     def get_success_url(self):
-        pass
+        return reverse('album_detail', self.kwargs)
 
 
 class SongCreateView(CreateView):
@@ -100,12 +105,16 @@ class SongCreateView(CreateView):
     template_name = 'music/song_add.html'
     action = _("Song is successfully added")
 
+
 class SongDetailView(DetailView):
     model = Song
     template_name = "music/song_detail.html"
 
     def get_object(self, queryset=None):
-        pass
+        return get_object_or_404(Song,
+                                 slug=self.kwargs["song"],
+                                 album__slug=self.kwargs["album"],
+                                 album__artist__profile__username=self.kwargs["username"])
 
 
 class SongUpdateView(LoginRequiredMixin, ActionMixin, UpdateView):
@@ -114,7 +123,10 @@ class SongUpdateView(LoginRequiredMixin, ActionMixin, UpdateView):
     action = _("Song is successfully updated")
 
     def get_object(self, queryset=None):
-        pass
+        return get_object_or_404(Song,
+                                 slug=self.kwargs["song"],
+                                 album__slug=self.kwargs["album"],
+                                 album__artist__profile__username=self.kwargs["username"])
 
     def get_success_url(self):
-        pass
+        return reverse('song_detail', self.kwargs)
